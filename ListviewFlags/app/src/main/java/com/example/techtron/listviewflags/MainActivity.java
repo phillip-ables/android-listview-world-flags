35fa4c3de019daa6c5a00f22080cbe59ceec234d
Phillip <phillipables1@gmail.com>
2018-02-09 22:48:04 -0500

    initalize Toolbar and ListView in main and set to findViewById's

--- ListviewFlags/app/src/main/java/com/example/techtron/listviewflags/MainActivity.java
+++ ListviewFlags/app/src/main/java/com/example/techtron/listviewflags/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.techtron.listviewflags;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    //initalize these views
+    android.support.v7.widget.Toolbar mToolbar;
+    ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //find your stuff R.id.stuff
+        mToolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
+        mListView = (ListView) findViewById(R.id.listview);
+        //we have the toolbar and the listview
+        //next, we set the value for the items
     }
 }
