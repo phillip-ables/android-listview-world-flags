eb21656feae145018bbfe5212946dbb68f0c8337
Phillip <phillipables1@gmail.com>
2018-02-09 23:45:51 -0500

    listview item layout with root element LinearLayout

--- ListviewFlags/app/src/main/java/com/example/techtron/listviewflags/MyAdapter.java
+++ ListviewFlags/app/src/main/java/com/example/techtron/listviewflags/MyAdapter.java
@@ -5,11 +5,12 @@ import android.support.annotation.NonNull;
 import android.widget.ArrayAdapter;
 
 /**
- * Created by user on 2/9/2018.
+ * Created by philables on 2/9/2018.
  */
 //error no default adapter availible -> click arrow and creat adapter -> choose the one that has the value of context and resource int
 
 public class MyAdapter extends ArrayAdapter<String> {
+    //source id is basically the custome id that will hold the image and the title
     public MyAdapter(@NonNull Context context, int resource) {
         super(context, resource);
     }
